21ebacdfa4083a11b62c8411b5ee03b1c1729a66
AnastasiiaTkachuk <anastasiia.tkachuk07@gmail.com>
2025-05-06 14:20:13 +0200

    add README.md

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Write your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Write your age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Write weight: ");
+        double weight = double.Parse(Console.ReadLine());
+        Console.WriteLine("Write your height: ");
+        double height = double.Parse(Console.ReadLine());
+        double bmi = BMI(weight, height);
+        Console.WriteLine($"Your profile:\n " +
+                          $"Full name: {firstName} {lastName}\n " +
+                          $"Age: {age}\n" +
+                          $" Weight: {weight}\n" +
+                          $" Height: {height}\n " +
+                          $"Body Mass Index: {bmi} ");
+    }
 
-Console.WriteLine("Hello, World!");
+    static double BMI(double weight, double height)
+    {
+        return weight / (height * height);
+    }
+}
